babbfded2423545fbdfd8fb4196068e90d0bb882
ptrkstr <11362913+ptrkstr@users.noreply.github.com>
2021-10-17 15:16:55 +0400

    Add missing Configuration initializer

--- Sources/Slab/Configuration.swift
+++ Sources/Slab/Configuration.swift
@@ -1,10 +1,14 @@
 import SwiftSoup
 
 public struct Configuration {
-    
+
     public static var `default`: Configuration {
         .init(modify: nil)
     }
     
     public let modify: ((_ element: Element, _ row: Int, _ column: Int) throws -> Element)?
+    
+    public init(modify: ((Element, Int, Int) throws -> Element)?) {
+        self.modify = modify
+    }
 }
